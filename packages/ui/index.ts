6480377350837d18a190bfdc184a6a327e1535a2
xcik1 <xcik1@fi.muni.cz>
2024-06-02 17:36:28 +0200

    feat(ui): shadcn tailwind integration

--- packages/ui/index.ts
+++
@@ -1,6 +0,0 @@
-// utils
-export { setupCounter } from "./src/utils/counter";
-
-// components
-export { Header } from "./src/components/header";
-export { Counter } from "./src/components/counter";
