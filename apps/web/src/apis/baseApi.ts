75a07472323ee70919b864fabd904570cd5b74f7
xcik1 <xcik1@fi.muni.cz>
2024-06-02 22:19:50 +0200

    feat(fe): add base axios instances

---
+++ apps/web/src/apis/baseApi.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8000";
+
+const axiosPublic = axios.create({
+  baseURL: BASE_URL,
+});
+
+const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
+
+const baseApi = {
+  axiosPublic,
+  axiosPrivate,
+};
+
+export default baseApi;
