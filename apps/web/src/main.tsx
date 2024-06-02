81e7a8cf339c7090295f3f02cd7ab168bc193f12
xcik1 <xcik1@fi.muni.cz>
2024-06-02 22:01:08 +0200

    feat(fe): basic app routing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,45 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import NotFoundPage from "./pages/NotFoundPage";
+import HomeLayout from "./layouts/HomeLayout";
+import HomePage from "./pages/HomePage";
+
 import "@repo/ui/styles.css";
 import "./globals.css";
+import CreateNotePage from "./pages/CreateNotePage";
+import EditNotePage from "./pages/EditNotePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/app",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "create/:noteId",
+        element: <CreateNotePage />,
+      },
+      {
+        path: "edit/:noteId",
+        element: <EditNotePage />,
+      },
+      {
+        path: "note/:noteId",
+        element: <NoteDetailPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
