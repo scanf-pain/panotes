29e0db2efc4f691be0bb495d216ae587e01ee4a1
xcik1 <xcik1@fi.muni.cz>
2024-06-02 16:57:35 +0200

    feat(fe): vite web app init

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@repo/ui/card";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <div className="bg-gray-900">app</div>
+      <Card title="sss"> dd</Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/ui/index.ts
@@ -0,0 +1,6 @@
+// utils
+export { setupCounter } from "./src/utils/counter";
+
+// components
+export { Header } from "./src/components/header";
+export { Counter } from "./src/components/counter";
