24aafbf5388e39490e3901c1e1fdf6429ae11fd1
xcik1 <xcik1@fi.muni.cz>
2024-06-02 22:00:28 +0200

    fix(fe): repair tailwind styles

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/*.tsx"],
+  content: ["./src/*.tsx", "./src/**/*.tsx"],
   presets: [sharedConfig],
 };
 
